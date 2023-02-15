2574cab8907cd902f35a3789f2b0d34140eaa76c
dg1223 <alavi1223@hotmail.com>
2023-02-15 01:04:25 -0500

    increment phone case quantity

---
+++ js/case.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberString = caseNumberField.value;
+  const previousCaseNumber = parseInt(caseNumberString);
+
+  const newCaseNumber = previousCaseNumber + 1;
+  caseNumberField.value = newCaseNumber;
+});
