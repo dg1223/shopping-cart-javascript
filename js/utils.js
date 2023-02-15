2c08cb7692383a6caf2aa3febcd45c1b96aed2d9
dg1223 <alavi1223@hotmail.com>
2023-02-15 05:11:14 -0500

    added docstrings

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,15 @@
+/**
+ * The getElementValue function returns the value of an element.
+ * 
+ *
+ * @param elementID Identify the element to be modified
+ * @param fieldType Determine which type of input field to update;
+ *                  accepted types: text or input 
+  
+ *
+ * @return The integer value of the element
+ *
+ */
 function getElementValue(elementID, fieldType) {
   const elementField = document.getElementById(elementID);
 
@@ -17,6 +29,18 @@ function getElementValue(elementID, fieldType) {
   return elementFieldValue;
 }
 
+/**
+ * The updateInputFieldNumber function updates the value of an input field
+ *
+ *
+ * @param isIncrease Determine whether the number is increased or decreased
+ * @param elementID Identify the element that needs to be updated
+ * @param fieldType Determine which type of input field to update;
+ *                  accepted types: text or input
+ *
+ * @return Total price
+ *
+ */
 function updateInputFieldNumber(isIncrease, elementID, fieldType) {
   const previousNumber = getElementValue(elementID, fieldType);
   const numberField = document.getElementById(elementID);
@@ -37,6 +61,20 @@ function updateInputFieldNumber(isIncrease, elementID, fieldType) {
   return newNumber;
 }
 
+/**
+ * The updateTextFieldNumber function updates the text field with the new number.
+ *
+ *
+ * @param isIncrease Determine whether the number is increased or decreased
+ * @param elementID Identify which element to update
+ * @param fieldType Determine which type of input field to update;
+ *                  accepted types: text or input
+ * @param basePrice The price of the phone
+ * @param inputFieldNumber Number of phones to buy
+ *
+ * @return Total price
+ *
+ */
 function updateTextFieldNumber(
   isIncrease,
   elementID,
@@ -52,6 +90,22 @@ function updateTextFieldNumber(
   return newNumber;
 }
 
+/**
+ * The updateTotal function updates the subtotal, tax, and total fields
+ * based on the values in the phone and case fields.
+ *
+ *
+ * @param subtotalElementID Get the value of the subtotal field
+ * @param taxElementID Get the value of the tax field
+ * @param totalElementID Get the value from the total field
+ * @param phoneElementID Get the value of the phone field
+ * @param caseElementID Get the value of the case field
+ * @param fieldType Determine which type of input field to update;
+ *                  accepted types: text or input
+ *
+ * @return nothing; sets the fields' values
+ *
+ */
 function updateTotal(
   subtotalElementID,
   taxElementID,
@@ -67,12 +121,15 @@ function updateTotal(
   const phoneTotal = getElementValue(phoneElementID, "text");
   const caseTotal = getElementValue(caseElementID, "text");
 
+  // Calculate subtotal
   const newSubTotal = phoneTotal + caseTotal;
   subtotalField.innerText = newSubTotal;
 
+  // Calculate tax
   const newTax = newSubTotal * 0.13;
   taxField.innerText = newTax.toFixed(2);
 
+  // Calculate total
   const newTotal = newSubTotal + newTax;
   totalField.innerText = newTotal;
 }
